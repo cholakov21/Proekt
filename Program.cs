0d087842b8bb5c0af29998e4111978f84df51010
unknown <eltostefanova10@gmail.com>
2026-06-12 10:01:16 +0300

    Proekt

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿namespace Zadacha
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split();
+            int[] arr = new int[input.Length];
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                arr[i] = int.Parse(input[i]);
+            }
+
+            int min = arr[0];
+            int max = arr[0];
+
+
+            for (int i = arr.Length - 1; i >= 0; i--)
+            {
+                Console.Write(arr[i]);
+
+                if (i > 0)
+                    Console.Write(" ");
+
+                if (arr[i] < min)
+                    min = arr[i];
+
+                if (arr[i] > max)
+                    max = arr[i];
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("МИН. ЕЛЕМЕНТ = " + min);
+            Console.WriteLine("МАКС. ЕЛЕМЕНТ = " + max);
+
+        }
+
+    }
+}
+
+}
